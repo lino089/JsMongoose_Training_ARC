cce6031d5349ea5ae7ff1069332411d0a4bec8a6
lino089 <marvellino089@gmail.com>
2026-02-19 01:09:39 +0700

    Ya gitulah, males jelasinnya

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
     console.log('Oh No Error');
     console.log(err);
 })
+
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const Amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'})
+
